eb941bafe00907e99857db104cb657d51572fba8
José Airton Neto <jose.neto.airton@gmail.com>
2025-05-23 16:30:35 -0300

    Fundamentos do DDD

---
+++ src/domain/use-cases/answeer-question.spec.ts
@@ -0,0 +1,13 @@
+import { AnswerQuestionUseCase } from './answer-question'
+
+test.skip('create an answer', () => {
+  const answerQuestion = new AnswerQuestionUseCase()
+
+  const answer = answerQuestion.execute({
+    instructorId: '1',
+    questionId: '1',
+    content: 'Nova resposta',
+  })
+
+  expect(answer.content).toEqual('Nova resposta')
+})
